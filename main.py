752cf7a64a8b440b88e4fa7d29a93b151f9bcd02
Artist: EternalEnvy <zhao.elton@gmail.com>
2017-11-07 16:51:36 -0500

    1.2: Split the run task to 3 part for other members to run.

--- main.py
+++ main.py
@@ -341,47 +341,51 @@ def main():
     ''' Fetch MNIST Data and USPS Data'''
     train_dataset, train_labels, raw_train_labels, valid_dataset, valid_labels, raw_valid_labels, test_dataset, test_labels, raw_test_labels = get_training_data()
     print("MNIST Data fetched success!")
-
-
     validation_usps, validation_usps_label = process_usps_data()
     print ("USPS Data fetched succesfully!")
 
-    ''' Train Logistic Regression Classifier'''
-    weights_lr, err_iteration_lr, train_accuracy_lr, validation_accuracy_lr = train_log_regression(train_dataset, train_labels, valid_dataset, valid_labels, raw_train_labels, raw_valid_labels)
-    print ("LR model trained")
-    
 
-    ''' Performance of LR on MNIST Data'''
-    plot_data(err_iteration_lr, 'loss', [0, 100, -2, -4], 'Iterations', 'Cross entropy loss', 'Loss change over iterations')
-    plot_data(train_accuracy_lr, 'loss', [0, 100, 0.8, 1], 'Iterations', 'Training Accuracy', 'Training accuracy change over iterations')
-    plot_data(validation_accuracy_lr, 'loss', [0, 100, 0.8, 1], 'Iterations', 'Validation Accuracy', 'Validation accuracy change over iterations')
-    pred_output_train_lr = np.dot(add_ones(train_dataset), weights_lr)
-    print ("Training Set Accuracy - Logistic Regression: ", accuracy(raw_train_labels, one_hot_encoding(pred_output_train_lr)))
-    pred_output_valid_lr = np.dot(add_ones(valid_dataset), weights_lr)
-    print ("Validation Set Accuracy - Logistic Regression: ", accuracy(raw_valid_labels, one_hot_encoding(pred_output_valid_lr)))
-    pred_output_test_lr = np.dot(add_ones(test_dataset), weights_lr)
-    print ("Test Set Accuracy - Logistic Regression: ", accuracy(raw_test_labels, one_hot_encoding(pred_output_test_lr)))
+    # TODO Generate result for Linear Regression model
+    if(False):
+        ''' Train Logistic Regression Classifier'''
+        weights_lr, err_iteration_lr, train_accuracy_lr, validation_accuracy_lr = train_log_regression(train_dataset, train_labels, valid_dataset, valid_labels, raw_train_labels, raw_valid_labels)
+        print ("LR model trained")
 
-    ''' Performance of LR on USPS Data'''
-    pred_output_usps_lr = np.dot(add_ones(validation_usps), weights_lr)
-    print ("USPS Accuracy - Logistic Regression: ", accuracy(validation_usps_label, one_hot_encoding(pred_output_usps_lr)))
+        ''' Performance of LR on MNIST Data'''
+        plot_data(err_iteration_lr, 'loss', [0, 100, -2, -4], 'Iterations', 'Cross entropy loss', 'Loss change over iterations')
+        plot_data(train_accuracy_lr, 'loss', [0, 100, 0.8, 1], 'Iterations', 'Training Accuracy', 'Training accuracy change over iterations')
+        plot_data(validation_accuracy_lr, 'loss', [0, 100, 0.8, 1], 'Iterations', 'Validation Accuracy', 'Validation accuracy change over iterations')
+        pred_output_train_lr = np.dot(add_ones(train_dataset), weights_lr)
+        print ("Training Set Accuracy - Logistic Regression: ", accuracy(raw_train_labels, one_hot_encoding(pred_output_train_lr)))
+        pred_output_valid_lr = np.dot(add_ones(valid_dataset), weights_lr)
+        print ("Validation Set Accuracy - Logistic Regression: ", accuracy(raw_valid_labels, one_hot_encoding(pred_output_valid_lr)))
+        pred_output_test_lr = np.dot(add_ones(test_dataset), weights_lr)
+        print ("Test Set Accuracy - Logistic Regression: ", accuracy(raw_test_labels, one_hot_encoding(pred_output_test_lr)))
 
-    ''' Train a Single Layer Neural Network '''
-    hidden_wts_nn, out_weights_nn, validation_accuracy_nn, train_losses_nn = train_single_layer_nn(train_dataset, train_labels, valid_dataset, raw_valid_labels)
-    print ("SNN model trained")
+        ''' Performance of LR on USPS Data'''
+        pred_output_usps_lr = np.dot(add_ones(validation_usps), weights_lr)
+        print ("USPS Accuracy - Logistic Regression: ", accuracy(validation_usps_label, one_hot_encoding(pred_output_usps_lr)))
+    # TODO Generate result for SNN
+    if(False):
+        ''' Train a Single Layer Neural Network '''
+        hidden_wts_nn, out_weights_nn, validation_accuracy_nn, train_losses_nn = train_single_layer_nn(train_dataset, train_labels, valid_dataset, raw_valid_labels)
+        print ("SNN model trained")
 
-    ''' Performance of Single Layer NN on MNIST Data '''
-    plot_data(train_losses_nn, 'loss', [0, 100, -5, 5], 'Iterations', 'Training Loss', 'Training Losses over epochs: Single layer NN')
-    plot_data(validation_accuracy_nn, 'accuracy', [0, 4, 0.9, 1], 'Iterations', 'Validation Accuracy', 'Validation accuracy over iterations Single layer NN')
-    test_accuracy_nn = evaluate_nn(test_dataset, raw_test_labels, hidden_wts_nn, out_weights_nn)
-    print ("Test set Accuracy - Single Layer NN: ", test_accuracy_nn)
+        ''' Performance of Single Layer NN on MNIST Data '''
+        plot_data(train_losses_nn, 'loss', [0, 100, -5, 5], 'Iterations', 'Training Loss', 'Training Losses over epochs: Single layer NN')
+        plot_data(validation_accuracy_nn, 'accuracy', [0, 4, 0.9, 1], 'Iterations', 'Validation Accuracy', 'Validation accuracy over iterations Single layer NN')
+        test_accuracy_nn = evaluate_nn(test_dataset, raw_test_labels, hidden_wts_nn, out_weights_nn)
+        print ("Test set Accuracy - Single Layer NN: ", test_accuracy_nn)
 
-    ''' Performance of Single Layer NN on USPS Data '''
-    usps_accuracy_nn = evaluate_nn(validation_usps, validation_usps_label, hidden_wts_nn, out_weights_nn)
+        ''' Performance of Single Layer NN on USPS Data '''
+        usps_accuracy_nn = evaluate_nn(validation_usps, validation_usps_label, hidden_wts_nn, out_weights_nn)
+    # TODO Generate result for CNN
+    if(False):
+        ''' Train a Convolutional Neural Network'''
+        print ("Training a CNN")
+        train_dataset_cnn, train_labels_cnn, valid_dataset_cnn, valid_labels_cnn, test_dataset_cnn, test_labels_cnn, usps_dataset_cnn, usps_labels_cnn = preprocess_data_cnn_tf(train_dataset, valid_dataset, test_dataset, raw_train_labels, raw_valid_labels, raw_test_labels,validation_usps, validation_usps_label)
+        train_cnn_model(train_dataset_cnn, train_labels_cnn, valid_dataset_cnn, valid_labels_cnn, test_dataset_cnn, test_labels_cnn, usps_dataset_cnn, usps_labels_cnn)
 
-    ''' Train a Convolutional Neural Network'''
-    print ("Training a CNN")
-    train_dataset_cnn, train_labels_cnn, valid_dataset_cnn, valid_labels_cnn, test_dataset_cnn, test_labels_cnn, usps_dataset_cnn, usps_labels_cnn = preprocess_data_cnn_tf(train_dataset, valid_dataset, test_dataset, raw_train_labels, raw_valid_labels, raw_test_labels,validation_usps, validation_usps_label)
-    train_cnn_model(train_dataset_cnn, train_labels_cnn, valid_dataset_cnn, valid_labels_cnn, test_dataset_cnn, test_labels_cnn, usps_dataset_cnn, usps_labels_cnn)
+    print("it's a disastah Meow~ >_<")
 
 main()
